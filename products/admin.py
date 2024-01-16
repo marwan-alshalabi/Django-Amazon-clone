2568116d26d6bc43555d96f893ded5fbbc27985c
marwan-alshalabi <alshalabimarwan1@gmail.com>
2024-01-16 20:01:22 +0100

    add admin customization

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product , Brand , Review , ProductImages
+
+class ProductImagesInline(admin.TabularInline):
+    model = ProductImages
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name','flag','price']
+    inlines = [ProductImagesInline]
+
+admin.site.register(Product,ProductAdmin)
+admin.site.register(Brand)
+admin.site.register(Review)
--- products/models.py
+++ products/models.py
@@ -3,10 +3,11 @@ from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import User
 
-FLAG_CHOICES = ()
-('New','New')
-('Sale','Sale')
-('Feature','Feature')
+FLAG_CHOICES = (
+    ('New','New'),
+    ('Sale','Sale'),
+    ('Feature','Feature'),
+)
 
 
 class Product(models.Model):

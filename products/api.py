621402d711b61f3d9f5da71f9acc4305d95c6b7c
marwan-alshalabi <alshalabimarwan1@gmail.com>
2024-02-11 18:00:05 +0100

    add resst API example & rest Swagger

---
+++ products/api.py
@@ -0,0 +1,20 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import generics
+
+from .serializers import Productserializers
+from .models import Product
+
+
+
+@api_view(["GET"])
+def product_list_api(request):
+    products = Product.objects.all() 
+    data = Productserializers(products,many=True).data 
+    return Response({'products':data})
+
+
+class ProductListAPI(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = Productserializers
+    
--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import ProductList,ProductDetail,BrandList,BrandDetail,add_product_review
 
+from .api import product_list_api , ProductListAPI
+
 
 urlpatterns =[
     path('' , ProductList.as_view()),
@@ -8,4 +10,8 @@ urlpatterns =[
     path('<slug:slug>', ProductDetail.as_view()),
     path('<slug:slug>/add-review', add_product_review),
     path('brands/<slug:slug>' , BrandDetail.as_view()),
+
+
+    path('api/list', product_list_api),
+    path('api/list2', ProductListAPI.as_view()),
 ]

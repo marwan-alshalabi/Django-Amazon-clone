08a27d2a53ddee4d025d246e60158ef791260608
marwan-alshalabi <alshalabimarwan1@gmail.com>
2024-02-13 18:35:48 +0100

    show brand detail in product serializer

--- products/serializers.py
+++ products/serializers.py
@@ -2,14 +2,18 @@ from rest_framework import serializers
 from .models import Product , Brand , Review , ProductImages
 
 
+class Brandserializers(serializers.ModelSerializer):
+    class Meta :
+        model = Brand
+        fields = '__all__'
+
+
+
 class Productserializers(serializers.ModelSerializer):
+    brand = Brandserializers ()
     class Meta :
         model = Product
         fields = '__all__'
 
 
 
-class Brandserializers(serializers.ModelSerializer):
-    class Meta :
-        model = Brand
-        fields = '__all__'

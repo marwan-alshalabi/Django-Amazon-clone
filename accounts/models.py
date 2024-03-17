90e248595b5e77a8aad49f270a1fda2325e44d84
Marwan Al Shalabi <alshalabimarwan1@gmail.com>
2024-03-17 18:15:36 +0100

    add User Address models

--- accounts/models.py
+++ accounts/models.py
@@ -3,5 +3,16 @@ from django.contrib.auth.models import User
 
 
 
+
+ADDRESS_TYPE = (
+    ('Home' , 'Home') ,
+    ('office' , 'Office') ,
+    ('Bussines' , 'Bussines',) ,
+    ('Other' , 'Other') ,
+)
+
+
 class Address(models.Model):
-    pass
+    user = models.ForeignKey(User,related_name='user_address',on_delete=models.CASCADE)
+    address = models.TextField(max_length=300)
+    type = models.CharField(max_length=20,choices=ADDRESS_TYPE)

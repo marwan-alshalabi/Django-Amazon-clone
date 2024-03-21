3b494cd6c8b205d684a54ee4058de687cb1ee23d
Marwan Al Shalabi <alshalabimarwan1@gmail.com>
2024-03-21 16:02:51 +0100

    add orders & checkout , process_payment , success , failed views structure

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path 
-from .views import add_to_cart
+from .views import add_to_cart,order_list,checkout,process_payment,payment_success,payment_failed
 
 urlpatterns = [
+    path('' , order_list),
+    path('checkout/' , checkout),
+    path('checkout/process-payment' , process_payment),
+    path('checkout/payment/success' , payment_success),
+    path('checkout/payment/failed' , payment_failed),
+    
     path('add-to-cart', add_to_cart),
 ]
--- orders/views.py
+++ orders/views.py
@@ -3,6 +3,37 @@ from .models import Cart , CartDetail
 from products.models import Product
 
 
+def order_list(request):
+
+    return render(request,'orders/orders.html',{})
+
+
+
+def checkout(request):
+    # review order ,apply coupon
+
+    return render(request,'orders/checkout.html',{})
+
+
+def process_payment(request):
+    #process payment $
+    pass
+
+
+def payment_success(request):
+    # if payment was success
+
+    code = ''
+
+    return render(request,'orders/success.html',{'code':code})
+
+
+def payment_failed(request):
+    # if payment was filed
+
+    return render(request,'orders/failed.html',{})
+
+
 def add_to_cart(request):
     product_id = request.POST['product_id']
     quantity = int(request.POST['quantity'])
